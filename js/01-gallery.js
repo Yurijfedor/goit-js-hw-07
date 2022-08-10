09d896adf8a0a3119d9f95bc5641ff7e663e6df6
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-08-11 00:19:47 +0300

    add lmlm

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }) => {
+  const markupGallery = `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+  galleryContainer.insertAdjacentHTML("beforeend", markupGallery);
+});
+
+galleryContainer.addEventListener("click", onGetRefOriginalImg);
+
+function onGetRefOriginalImg(evt) {
+  evt.preventDefault();
+  const dataOfImg = evt.target.hasAttribute("data-source");
+  if (!dataOfImg) {
+    return;
+  }
+  const originalImgRef = evt.target.dataset.source;
+  console.log(evt.target.src);
+  const instance = basicLightbox.create(`
+    <img src=${originalImgRef}>
+`);
+
+  instance.show();
+  return originalImgRef;
+}
